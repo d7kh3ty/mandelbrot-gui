f47596eb48b0d1556e94a9c10c8bb12b6ebfaca6
Izzy <cassell.izzy@gmail.com>
2021-07-29 16:59:19 +0100

    multhreading is very epic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate image;
-use std::thread;
-use std::sync::{Arc, Mutex};
-
 use image::{ImageBuffer, Rgb};
+
+use std::thread::{self, JoinHandle};
+use std::sync::mpsc;
 
 /*
 fn julia(a: f32, b: f32, ca: f32, cb: f32, i: i32, max: i32) -> i32 {
@@ -40,22 +40,21 @@ fn mandel(dx: f32, dy: f32, max: i32) -> i32 {
     i
 }
 
-static IMGX: u32 = 1000;
-static IMGY: u32 = 1000;
+static IMGX: u32 = 5000;
+static IMGY: u32 = 5000;
 
 static POSX: f32 = -0.70;
 static POSY: f32 = 0.26109119081845;
 
-static ITERATIONS: i32 = 1000;
+static ITERATIONS: i32 = 5000;
 
 static SCALE: f32 = 100.0;
 
-//fn gen(x1: u32, x2: u32, y1: u32, y2: u32, imgbuf: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
-fn gen(x1: u32, x2: u32, y1: u32, y2: u32, m: Arc<Mutex<ImageBuffer<Rgb<u8>, Vec<u8>>>>) {
+fn gen(x1: u32, x2: u32, y1: u32, y2: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     // generate the fractal
+    let mut imgbuf = image::ImageBuffer::new(IMGX, IMGY);
     for x in x1..x2 {
-        let mut imgbuf = m.lock().unwrap();
-        print!("{}%     \r", (x as f32 / x2 as f32) * 100.0);
+        print!("{:.2}%     \r", (x as f32 / x2 as f32) * 100.0);
         for y in y1..y2 {
             //let dx: f32 = (x as f32 / IMGX as f32) as f32;
             //let dy: f32 = (y as f32 / IMGY as f32) as f32;
@@ -77,80 +76,59 @@ fn gen(x1: u32, x2: u32, y1: u32, y2: u32, m: Arc<Mutex<ImageBuffer<Rgb<u8>, Vec
                 *pixel = image::Rgb([i, 0, i]);
             }
         }
-        imgbuf.save("assets/temp.png");
     }
+    println!("{}, {}, {}, {}", x1, x2, y1, y2);
+    imgbuf
 }
 
-fn main() {
-    // Create a new ImgBuf with width: IMGX and height: IMGY
-    //let mut imgbuf = image::ImageBuffer::new(IMGX, IMGY);
-
-    let m = Arc::new(Mutex::new(image::ImageBuffer::new(IMGX, IMGY)));
+fn spawn(n: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let (tx, rx) = mpsc::channel();
+    let mut xm: u32 = 0;
+    let mut ym: u32 = 0;
+    for i in 1..n {
+        if n % i == 0 {
+            xm = i;
+            ym = n / i;
+        }
+    }
+    let sx = IMGX / xm;
+    let sy = IMGY / ym;
     let mut threads = vec![];
-
-    {
-        let m = Arc::clone(&m);
-        threads.push(thread::spawn(move || {
-            gen(0, IMGX / 2, 0, IMGY / 2, m);
-        }));
+    for x in 0..xm {
+        for y in 0..ym {
+            //println!("{}, {}, {}, {}", x * sx, x * sx + sx, y * sy, y * sy + sy);
+            //gen(x * IMGX / xm, IMGX / xm, 0, IMGY / 2);
+            {
+                let t = tx.clone();
+                threads.push(thread::spawn(move || {
+                    let f = gen(x * sx, x * sx + sx, y * sy, y * sy + sy);
+                    t.send(f).unwrap();
+                }));
+            }
+        }
     }
-
-    {
-        let m = Arc::clone(&m);
-        threads.push(thread::spawn(move || {
-            gen(IMGX / 2, IMGX, 0, IMGY / 2, m);
-        }));
-    }
-    {
-        let m = Arc::clone(&m);
-        threads.push(thread::spawn(move || {
-            gen(0, IMGX / 2, IMGY / 2, IMGY, m);
-        }));
-    }
-    {
-        let m = Arc::clone(&m);
-        threads.push(thread::spawn(move || {
-            gen(IMGX / 2, IMGX, IMGY / 2, IMGY, m);
-        }));
-    }
-
     for thread in threads {
         thread.join().unwrap();
     }
 
-    let imgbuf = m.lock().unwrap();
+    let mut imgbuf = image::ImageBuffer::new(IMGX, IMGY);
 
-    // Save the image as “fractal.png”, the format is deduced from the path
+    for mut img in rx.iter().take(n as usize) {
+        println!("done");
+        for (x, y, p) in img.enumerate_pixels_mut() {
+            let pixel = imgbuf.get_pixel_mut(x, y);
+            let image::Rgb(data) = *p;
+            if data[0] > 0 || data[1] > 0 || data[2] > 0 {
+                *pixel = *p;
+            }
+        }
+    }
+    imgbuf
+}
+
+fn main() {
+    //let mut threads = vec![];
+
+    let imgbuf = spawn(128);
     imgbuf.save("assets/fractal.png").unwrap();
 }
-
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn basic_real() {
-        assert_eq!(mandel(1.0, 0.0, 1.0, 1, 100), 2);
-    }
-    #[test]
-    fn zero() {
-        assert_eq!(mandel(0.0, 0.0, 1.0, 1, 100), 3);
-    }
-    #[test]
-    fn iterative() {
-        assert_eq!(mandeli(1.0, 0.0, 100), 2);
-    }
-    #[test]
-    fn iterative_zero() {
-        assert_eq!(mandeli(0.0, 0.0, 100), 3);
-    }
-    #[test]
-    fn stays_small() {
-        assert_eq!(mandel(0.0, 0.0, -1.0, 1, 10), 10);
-    }
-    #[test]
-    fn iterative_stays_small() {
-        assert_eq!(mandeli(0.0, 0.0, -1.0, 10), 10);
-    }
-}
-*/
