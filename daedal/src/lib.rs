f8ff5033e4fde211a273e46f387507447f58e6dd
izzy <izzy@pop-os.localdomain>
2022-02-03 19:39:58 +0000

    thank god it still works

--- daedal/src/lib.rs
+++ daedal/src/lib.rs
@@ -1,8 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+// import mandelbrot
+pub mod mandelbrot;
+pub use crate::mandelbrot::*;
+
+// import julia
+pub mod julia;
+pub use crate::julia::*;
+
+// import options
+pub mod options;
+pub use crate::options::*;
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-// import mandelbrot
-pub mod mandelbrot;
-pub use crate::mandelbrot::*;
-
-// import julia
-pub mod julia;
-pub use crate::julia::*;
